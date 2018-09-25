3b668a8b6f0fe12783d7d74887d9238c0934e82e
Irina Trotsenko <irina.muc43@gmail.com>
2018-09-25 15:32:56 +0200

    Irina_Trotsenko corrected minor synthax errors

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 from dna import DNA
 import pytest
 
-def bad_seq_raises_error():
-    with pytest.raises(ValuError):
+def test_bad_seq_raises_error():
+    with pytest.raises(ValueError):
         DNA('ATB')
 
 
@@ -12,3 +12,4 @@ def test_complem_seq_works():
     assert DNA('ATC').complimentary_sequence == DNA('TAG')
 
 #if __name__=='__main__':      # what's below only active if not called as import module dna
+#every funct should start with test_

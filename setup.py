d65e9b33da46e921d372f9cc74bd3bad35b1f943
Irina Trotsenko <irina.muc43@gmail.com>
2018-09-25 17:36:25 +0200

    rna.py , other help files added for tracking

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='genomics_demo',
+    version='v0.0.1',
+    url='',
+    packages=['genomics_demo'],
+    license='',
+    author='irina',
+    author_email='',
+    description=''
+)

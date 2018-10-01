40e7f3172e86ec7023ee764ecc99300bef42d32f
Irina Trotsenko <irina.muc43@gmail.com>
2018-10-01 13:51:38 +0200

    method check_triplet added and test_DNA updated

--- tests/test_DNA.py
+++ tests/test_DNA.py
@@ -11,5 +11,8 @@ def test_complem_seq_works():
     assert DNA('GTC').complimentary_sequence == DNA('CAG')
     assert DNA('ATC').complimentary_sequence == DNA('TAG')
 
+def test_triplet_works():
+    assert DNA('GTCAAATTTGGG').test_triplet== DNA('4 codons')
+
 #if __name__=='__main__':      # what's below only active if not called as import module dna
 #every funct should start with test_
